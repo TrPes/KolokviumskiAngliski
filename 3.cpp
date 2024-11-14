9ed378e6c6895b0bca219577a7ee762a6db52119
Trpeskiii <126724325+TrPes@users.noreply.github.com>
2024-11-14 18:57:39 +0100

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+   int m;
+   cin>>m;
+
+
+   for(int i=1;i<=m;i++) {
+      cout<<"+";
+      for(int j=2;j<=m-1;j++) {
+         if(i==1 || i==m) {
+            cout<<"+";
+         }
+         else if(j>1 && j<=m-1) {
+            cout<<"-";
+         }
+
+      }
+      cout<<"+"<<endl;
+   }
+
+
+
+
+   return 0;
+}
